0ed293131a9cc80a339acd109537bd75e700d470
Madeline Urbanic <madelinerurbanic@gmail.com>
2023-10-02 18:49:45 -0400

    redo fetch

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Weather-Locator
 webpage that shows weather in each city
+
+https://reqbin.com/code/javascript/wc3qbk0b/javascript-fetch-json-example
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,15 @@ var apiKey = 'c6131f59ee081d22e22aae67aa4ed7ac';
 var city = 'Atlanta';
 var display;
 var weatherData;
+var data;
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=10&lon=10&appid=" + apiKey;
 
+
 fetch(queryURL, {
-    method: 'GET', //GET is the default.
-    credentials: 'same-origin', // include, *same-origin, omit
-    redirect: 'follow', // manual, *follow, error
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-});
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+    },
+})
+   .then(response => response.json())
+   .then(response => console.log(JSON.stringify(response)))
